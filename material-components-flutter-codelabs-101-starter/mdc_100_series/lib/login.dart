ff429fc259fdd1929266b9b19db2ac20a209f114
John Taylor <jaydeetay@users.noreply.github.com>
2018-11-25 23:25:02 -0500

    First part of codelab.

--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
@@ -20,7 +20,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
+  final  _passwordController = TextEditingController();
+  final  _usernameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,11 +39,28 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             SizedBox(height: 120.0),
+            TextField(
+                controller: _usernameController,
+                decoration:
+                    InputDecoration(filled: true, labelText: "Username")),
+            SizedBox(height: 12.0),
+            TextField(
+                controller: _passwordController,
+                decoration:
+                    InputDecoration(filled: true, labelText: "Password"),
+                obscureText: true),
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
+            ButtonBar(children: <Widget>[
+              FlatButton(child: Text("CANCEL"), onPressed: () {
+                _usernameController.clear();
+                _passwordController.clear();
+              }),
+              RaisedButton(child: Text("NEXT"), onPressed: () {
+                Navigator.pop(context);
+              }),
+            ])
           ],
         ),
       ),
