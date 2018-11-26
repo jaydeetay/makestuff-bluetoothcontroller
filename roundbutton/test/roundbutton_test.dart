dd3bd2d18b6a4f60e76d56316780be8f4c3018f4
John Taylor <jaydeetay@users.noreply.github.com>
2018-11-25 22:53:35 -0500

    Minor tweaks.

--- roundbutton/test/roundbutton_test.dart
+++ roundbutton/test/roundbutton_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:roundbutton/roundbutton.dart';
-//import 'package:test/test.dart';
 
 void main() {
   test('adds one to input values', () {
